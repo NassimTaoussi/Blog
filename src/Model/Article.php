d159d87e0ff336a4a27eab52201b1ae32aeaa1b4
Nassim Taoussi <nassim-taoussi@hotmail.com>
2022-06-16 00:56:33 +0200

    fix: créations des getters dans la classe article pour afficher les données dans les items des posts

--- src/Model/Article.php
+++ src/Model/Article.php
@@ -22,6 +22,79 @@ class Article {
         $this->chapo = $chapo;
         $this->picture = $picture;
     }
+    
+    /* GETTERS and SETTERS */
+
+    // Id
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    // Author
+
+    public function setAuthor($author) {
+        $this->author = $author;
+    }
+
+    public function getAuthor() {
+        return $this->author;
+    }
+
+    // Title
+
+    public function setTitle($title) {
+        $this->title = $title ;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    // MajDate
+
+    public function setMajDate($majDate) {
+        $this->majDate = $majDate ;
+    }
+
+    public function getMajDate() {
+        return $this->majDate ;
+    }
+
+    // Content
+
+    public function setContent($content) {
+        $this->content = $content ;
+    }
+
+    public function getContent() {
+        return $this->content ;
+    }
+
+    // Chapo
+
+    public function setChapo($chapo) {
+        $this->chapo = $chapo ;
+    }
+
+    public function getChapo() {
+        return $this->chapo ;
+    }
+
+    // Picture
+
+    public function setPicture($picture) {
+        $this->picture = $picture ;
+    }
+
+    public function getPicture() {
+        return $this->picture ;
+    }
+
 }
 
 ?>
