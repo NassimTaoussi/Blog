16e8294a97feffa3e317c3a4743ff50df3362c3c
Nassim Taoussi <nassim-taoussi@hotmail.com>
2022-08-16 23:54:42 +0200

    refactor: insertion d'un commentaire lié à l'article

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -35,24 +35,26 @@ class CommentRepository extends ModelRepository {
      * 
      * @return array 
     */
-    public function findComments($start, $length): array
+    public function findComments($start, $length, $id): array
     {
-        $result = $this->pdo->query('SELECT comment.id, date_of_post, username, content, comment.valid FROM comment INNER JOIN user ON comment.author = user.id LIMIT ' . $start . ',' . $length);
+        $result = $this->pdo->query('SELECT comment.id, date_of_post, username, content, comment.valid, comment.article FROM comment INNER JOIN user ON comment.author = user.id WHERE comment.article = ' . $id . ' LIMIT ' . $start . ',' . $length);
         $comments = $result->fetchAll();
         $comments = array_map(function($comment) {
-            return new Comment($comment['id'], $comment['username'], new \DateTime($comment['date_of_post']), $comment['content'], $comment['valid']);
+            return new Comment($comment['id'], $comment['username'], new \DateTime($comment['date_of_post']), $comment['content'], $comment['valid'], $comment['article']);
         }, $comments);
         return $comments;
     }
 
     public function insertComment($comment) {
-        $sql = "INSERT INTO comment (author, date_of_post, content, valid) 
-                VALUES(:author, :dateOfPost, :content, :valid)";
+        $sql = "INSERT INTO comment (author, date_of_post, content, valid, article) 
+                VALUES(:author, :dateOfPost, :content, :valid, :article)";
         $query = $this->pdo->prepare($sql);
         $query->execute(array(':author'=> $comment->getAuthor(),
         ':dateOfPost'=> $comment->getDateOfPost()->format('Y-m-d H:i:s'),
         ':content'=> $comment->getContent(),
-        ':valid' => $comment->getValid()));
+        ':valid' => $comment->getValid(),
+        ':article' => $comment->getArticle(),
+        ));
 
 
     }
