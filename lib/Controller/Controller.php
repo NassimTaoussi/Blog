9279cc993d10ec5b0730246c99fbfa8325492e35
Nassim Taoussi <nassim-taoussi@hotmail.com>
2022-10-09 23:00:06 +0200

    feat: creation de la fonction denyAccessUnlessGranted

--- lib/Controller/Controller.php
+++ lib/Controller/Controller.php
@@ -2,12 +2,12 @@
 
 namespace NTaoussi\Lib\Controller;
 
+use NTaoussi\Lib\Exception\AccessDeniedException;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
-
 abstract class Controller {
 
-    public function render(string $template, array $context = []) {
+    public static function render(string $template, array $context = []) {
         $loader = new FilesystemLoader(ROOT.'/template');
 
         $twig = new Environment($loader);
@@ -15,9 +15,42 @@ abstract class Controller {
         $twig->display($template, $context);
     }
 
-    public function redirect(string $url) {
+    public static function redirect(string $url) {
         header('Location: '. $url);
         exit();
+    }
+
+    public static function denyAccessUnlessGranted($valid, $admin)
+    {
+        if($valid == 1) 
+        {
+            if($admin == 1) 
+            {
+
+            }
+            else 
+            { 
+                throw new AccessDeniedException();
+            }
+        }
+        else 
+        {
+            throw new AccessDeniedException();
+        }
+    }
+
+    public function setFlash(string $message, string $type) {
+        $_SESSION['user']['flash'] = [
+            'message' => $message,
+            'type' => $type,
+        ] ;
+    }
+
+    public function flash() {
+        if(isset($_SESSION['user']['flash']))
+        {
+            unset($_SESSION['user']['flash']);
+        }
     }
 
 }
